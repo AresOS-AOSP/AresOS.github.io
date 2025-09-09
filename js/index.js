aa0b91e31c2607c3a2f3f2faffa7335febe34254
ChrisCatto <chriscatto@proton.me>
2025-09-09 10:55:39 +0300

    Major cleanup and rebrand to AresOS
    Change-Id: Ie0f3932d5f1195d0ae067bce9267f965b07db7f3
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- js/index.js
+++ js/index.js
@@ -5,19 +5,6 @@ document.querySelector(".logo").addEventListener("click", function (e) {
 
 function onUrlChange() {
   const hash = window.location.hash;
-  const downloadsRegex = /^#downloads(?:_(.+))?$/;
-  const match = hash.match(downloadsRegex);
-
-  if (match) {
-    const baseSrc = "download.html#downloads";
-    if (match[1]) {
-      const rawSearch = match[1];
-      const searchText = decodeURIComponent(rawSearch.replace(/_/g, " "));
-      localStorage.setItem("deviceSearchText", searchText);
-    }
-    animateIframe(baseSrc);
-    return;
-  }
 
   const routeMap = {
     "#about": "home.html#about",
